d5830e5f80ca535aaebe26313b6eb5dd6cb61713
Nischal <nischal.aremanda1432@gmail.com>
2022-01-07 10:15:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById(
+      "demo").innerHTML = "JavaScript Statements The programming instructions written in a program in a programming language are known as statements.";
+}
